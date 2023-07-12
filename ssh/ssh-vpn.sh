cf3c407aa3c943b4a95c62b04e52d145e05c32cf
muslimsiber <138317412+muslimsiber@users.noreply.github.com>
2023-07-12 14:58:23 +0800

    ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -380,6 +380,227 @@ END
 service cron restart >/dev/null 2>&1
 service cron reload >/dev/null 2>&1
 
+#install squid
+
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+# ==================================================
+# Link Hosting Kalian
+muslimsiberr="raw.githubusercontent.com/muslimsiber/aws/main/ssh"
+
+# initializing var
+export DEBIAN_FRONTEND=noninteractive
+MYIP=$(wget -qO- ipinfo.io/ip);
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+NET=$(ip -o $ANU -4 route show to default | awk '{print $5}');
+source /etc/os-release
+ver=$VERSION_ID
+
+# go to root
+cd
+
+# install squid
+cd
+apt -y install squid3
+wget -O /etc/squid/squid.conf "https://${muslimsiberr}/squid3.conf"
+sed -i $MYIP2 /etc/squid/squid.conf
+
+#import ovpnmenu
+
+# download script
+cd /usr/bin
+
+# menu ssh ovpn
+wget -O addssh "https://raw.githubusercontent.com/muslimsiber/aws/main/ssh/addssh.sh"
+wget -O trialssh "https://raw.githubusercontent.com/muslimsiber/aws/main/ssh/trialssh.sh"
+wget -O renewssh "https://raw.githubusercontent.com/muslimsiber/aws/main/ssh/renewssh.sh"
+wget -O cekssh "https://raw.githubusercontent.com/muslimsiber/aws/main/ssh/cekssh.sh"
+wget -O member "https://raw.githubusercontent.com/muslimsiber/aws/main/ssh/member.sh"
+wget -O delssh "https://raw.githubusercontent.com/muslimsiber/aws/main/ssh/delssh.sh"
+wget -O delexp "https://raw.githubusercontent.com/muslimsiber/aws/main/ssh/delexp.sh"
+wget -O autokill "https://raw.githubusercontent.com/muslimsiber/aws/main/ssh/autokill.sh"
+wget -O ceklim "https://raw.githubusercontent.com/muslimsiber/aws/main/ssh/ceklim.sh"
+wget -O restart "https://raw.githubusercontent.com/muslimsiber/aws/main/ssh/restart.sh"
+wget -O sshovpnmenu "https://raw.githubusercontent.com/muslimsiber/aws/main/update/sshovpn.sh"
+
+chmod +x addssh
+chmod +x trialssh
+chmod +x renewssh
+chmod +x cekssh
+chmod +x member
+chmod +x delssh
+chmod +x delexp
+chmod +x autokill
+chmod +x ceklim
+chmod +x restart
+chmod +x sshovpnmenu
+
+#install Ovpn
+
+# My Telegram : https://t.me/fiexie
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+clear
+# By Akbar Maulana
+# ==================================================
+# Link Hosting Kalian
+muslimsiber="raw.githubusercontent.com/muslimsiber/aws/main/ssh"
+
+# initialisasi var
+export DEBIAN_FRONTEND=noninteractive
+OS=`uname -m`;
+MYIP=$(wget -qO- ipinfo.io/ip);
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+ANU=$(ip -o $ANU -4 route show to default | awk '{print $5}');
+
+# Install OpenVPN dan Easy-RSA
+apt install openvpn easy-rsa unzip -y
+apt install openssl iptables iptables-persistent -y
+mkdir -p /etc/openvpn/server/easy-rsa/
+cd /etc/openvpn/
+wget https://${muslimsiber}/vpn.zip
+unzip vpn.zip
+rm -f vpn.zip
+chown -R root:root /etc/openvpn/server/easy-rsa/
+
+cd
+mkdir -p /usr/lib/openvpn/
+cp /usr/lib/x86_64-linux-gnu/openvpn/plugins/openvpn-plugin-auth-pam.so /usr/lib/openvpn/openvpn-plugin-auth-pam.so
+
+# nano /etc/default/openvpn
+sed -i 's/#AUTOSTART="all"/AUTOSTART="all"/g' /etc/default/openvpn
+
+# restart openvpn dan cek status openvpn
+systemctl enable --now openvpn-server@server-tcp
+systemctl enable --now openvpn-server@server-udp
+/etc/init.d/openvpn restart
+/etc/init.d/openvpn status
+
+# aktifkan ip4 forwarding
+echo 1 > /proc/sys/net/ipv4/ip_forward
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+
+# Buat config client TCP 1194
+cat > /etc/openvpn/tcp.ovpn <<-END
+client
+dev tun
+proto tcp
+remote xxxxxxxxx 1194
+resolv-retry infinite
+route-method exe
+nobind
+persist-key
+persist-tun
+auth-user-pass
+comp-lzo
+verb 3
+END
+
+sed -i $MYIP2 /etc/openvpn/tcp.ovpn;
+
+# Buat config client UDP 2200
+cat > /etc/openvpn/udp.ovpn <<-END
+client
+dev tun
+proto udp
+remote xxxxxxxxx 2200
+resolv-retry infinite
+route-method exe
+nobind
+persist-key
+persist-tun
+auth-user-pass
+comp-lzo
+verb 3
+END
+
+sed -i $MYIP2 /etc/openvpn/udp.ovpn;
+
+# Buat config client SSL
+cat > /etc/openvpn/ssl.ovpn <<-END
+client
+dev tun
+proto tcp
+remote xxxxxxxxx 990
+resolv-retry infinite
+route-method exe
+nobind
+persist-key
+persist-tun
+auth-user-pass
+comp-lzo
+verb 3
+END
+
+sed -i $MYIP2 /etc/openvpn/ssl.ovpn;
+
+cd
+# pada tulisan xxx ganti dengan alamat ip address VPS anda 
+/etc/init.d/openvpn restart
+
+# masukkan certificatenya ke dalam config client TCP 1194
+echo '<ca>' >> /etc/openvpn/tcp.ovpn
+cat /etc/openvpn/server/ca.crt >> /etc/openvpn/tcp.ovpn
+echo '</ca>' >> /etc/openvpn/tcp.ovpn
+
+# Copy config OpenVPN client ke home directory root agar mudah didownload ( TCP 1194 )
+cp /etc/openvpn/tcp.ovpn /home/vps/public_html/tcp.ovpn
+
+# masukkan certificatenya ke dalam config client UDP 2200
+echo '<ca>' >> /etc/openvpn/udp.ovpn
+cat /etc/openvpn/server/ca.crt >> /etc/openvpn/udp.ovpn
+echo '</ca>' >> /etc/openvpn/udp.ovpn
+
+# Copy config OpenVPN client ke home directory root agar mudah didownload ( UDP 2200 )
+cp /etc/openvpn/udp.ovpn /home/vps/public_html/udp.ovpn
+
+# masukkan certificatenya ke dalam config client SSL
+echo '<ca>' >> /etc/openvpn/ssl.ovpn
+cat /etc/openvpn/server/ca.crt >> /etc/openvpn/ssl.ovpn
+echo '</ca>' >> /etc/openvpn/ssl.ovpn
+
+# Copy config OpenVPN client ke home directory root agar mudah didownload ( SSL )
+cp /etc/openvpn/ssl.ovpn /home/vps/public_html/ssl.ovpn
+
+#firewall untuk memperbolehkan akses UDP dan akses jalur TCP
+
+iptables -t nat -I POSTROUTING -s 10.6.0.0/24 -o $ANU -j MASQUERADE
+iptables -t nat -I POSTROUTING -s 10.7.0.0/24 -o $ANU -j MASQUERADE
+iptables-save > /etc/iptables.up.rules
+chmod +x /etc/iptables.up.rules
+
+iptables-restore -t < /etc/iptables.up.rules
+netfilter-persistent save
+netfilter-persistent reload
+
+# Restart service openvpn
+systemctl enable openvpn
+systemctl start openvpn
+/etc/init.d/openvpn restart
+
 # remove unnecessary files
 sleep 0.5
 echo -e "[ ${green}INFO$NC ] Clearing trash"
